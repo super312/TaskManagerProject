85838bc5f281ace7fd27b31494b7c9bd4ba20e5c
super312 <brunocesarmeneguzzuzamarchi@gmail.com>
2023-08-01 14:30:12 -0300

    main settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import Navbar from './navbar/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <h1>Hello World</h1>
     </>
   )
---
+++ src/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css'
+
+export default function Navbar() {
+    return(
+        <div className='navbar'>
+            <logo>Logo</logo>
+            <input placeholder='search' />
+            <profile>Profile</profile>
+        </div>
+    )
+}
